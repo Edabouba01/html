5f5afebd38d365c044378e0844ac93c8e29733e4
Edabouba01 <Marikoben111@gmail.com>
2023-11-17 12:46:25 -0400

    fini

--- fichier.js
+++ fichier.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
-    // Fonction pour mettre à jour le contenu d'un élément div
+    // mis a jour des donnee pour les html
     function updateData(elementId, data) {
-        $('#' + elementId).html('Données reçues : ' + JSON.stringify(data));
+        $('#' + elementId).html('Données reçues : ' + JSON.stringify(data)); 
     }
 
     // Requêtes AJAX
     $.ajax({
         url: 'http://127.0.0.1:8000/temperatur/',
         method: 'GET',
-        dataType: 'json',
+        dataType: 'text',
         success: function(data) {
             updateData('temperatur-data', data);
         },
         error: function(error) {
-            console.error('Erreur lors de la récupération des données:', error);
+            console.error('donnee temperature non recuperer', error);
         }
     });
 
     $.ajax({
         url: 'http://127.0.0.1:8000/humidite/',
         method: 'GET',
-        dataType: 'json',
+        dataType: 'text',
         success: function(data) {
             updateData('humidite-data', data);
         },
         error: function(error) {
-            console.error('Erreur lors de la récupération des données:', error);
+            console.error('donnee humidite non recuperer', error);
         }
     });
 
     $.ajax({
         url: 'http://127.0.0.1:8000/pression/',
         method: 'GET',
-        dataType: 'json',
+        dataType: 'text',
         success: function(data) {
 
             updateData('pression-data', data);
         },
         error: function(error) {
-            console.error('Erreur lors de la récupération des données:', error);
+            console.error('donnee pression non recuperer', error);
         }
     });
 });
